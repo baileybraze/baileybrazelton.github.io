78d468471761b21530970e63750e96e74db085a4
baileybraze <bailey.braze@gmail.com>
2020-01-11 20:10:30 -0700

    PLEASE WORK

--- baileybrazelton.github.io/myScript.js
+++
@@ -1,6 +0,0 @@
-function upDates() {
-    document.getElementById("currentyear").innerHTML = new Date().getFullYear();
-    let modif = new Date(document.lastModified);
-    let modified = modif.toLocaleString();
-    document.getElementById("update").innerHTML = "Last Updated " + modified;
-}
