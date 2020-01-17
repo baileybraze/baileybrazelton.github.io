1874d29981cb8edeb4e9a812e650c39a73fda2e2
baileybraze <bailey.braze@gmail.com>
2020-01-16 20:26:19 -0700

    commit

--- myScript.js
+++ myScript.js
@@ -1,6 +1,5 @@
-function upDates() {
-    document.getElementById("currentyear").innerHTML = new Date().getFullYear();
-    let modif = new Date(document.lastModified);
-    let modified = modif.toLocaleString();
-    document.getElementById("update").innerHTML = "Last Updated " + modified;
-}
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+let modif = new Date(document.lastModified);
+let modified = modif.toLocaleString();
+document.getElementById("update").innerHTML = "Last Updated " + modified;
